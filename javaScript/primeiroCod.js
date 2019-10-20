a3a6053399927d6a2fc34816ad655e5c1c3d508b
afranioag <afrani.garcia@ccc.ufcg.edu.br>
2019-10-20 18:06:24 -0300

    aprendizado em js continua

--- javaScript/primeiroCod.js
+++ javaScript/primeiroCod.js
@@ -2,11 +2,25 @@ function cria_pessoa (nome){
 	let pessoa = {}
 	pessoa.nome = nome;
 	pessoa.fale = function(){
-		return "oi eu sou" + pessoa.nome
+		return "oi eu sou " + pessoa.nome
 	};
 	return pessoa;
 }
 
 pessoa = cria_pessoa("abril");
-
 console.log(pessoa.fale());
+
+
+function cria_disciplina(id, nome){
+	let dis = {}
+	dis.id = id;
+	dis.nome = nome;
+	pessoa = cria_pessoa("joan")
+	console.log(pessoa.fale())	
+
+	dis.dados = function(){return dis.id +" id; " + dis.nome +" nome"}
+	return dis
+}
+
+d = cria_disciplina(021, "prog")
+console.log(d.dados())
